2e155b5e62134d25405279dd5f3f94a0598a16a3
Martin Masevski <martin.masevski@gmail.com>
2023-02-03 23:24:38 +0100

    feat: end lesson 9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,24 @@
 
   export let data: PageData;
 
+  let form = {
+    searchString: "",
+  };
+
+  let searchString = "";
+
+  $: selectedMonsters = data.monsters.filter((monster) => {
+    return monster.name.toLowerCase().includes(searchString.toLowerCase());
+  });
+
   $: monsterId = $page.url.searchParams.get("monsterId") || "";
   $: monster = data.monsters.find((monster) => monster.id === monsterId);
   $: monsterId2 = $page.url.searchParams.get("monsterId2") || "";
   $: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
+
+  const submitSearch = (e: Event) => {
+    searchString = form.searchString;
+  };
 </script>
 
 {#if monster}
@@ -25,8 +39,18 @@
   {/each}
 </div>
 
+<form class="search-form" on:submit={submitSearch}>
+  <input
+    class="search-field"
+    type="text"
+    bind:value={form.searchString}
+    placeholder="Pokemon Name"
+  />
+  <input type="submit" value="Search" />
+</form>
+
 <div class="monsters">
-  {#each data.monsters as monster (monster.id)}
+  {#each selectedMonsters as monster (monster.id)}
     <Monster {monster} isInteractive />
   {/each}
 </div>
@@ -53,5 +77,25 @@
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
+  }
+  .search-form {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+  }
+  .search-form input[type="text"] {
+    padding: 5px 10px;
+    width: 200px;
+    border-radius: 5px;
+    border: 1px solid #333;
+  }
+
+  .search-form input[type="submit"] {
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: 1px solid #333;
+    background-color: #333;
+    color: #fff;
+    margin-left: 10px;
   }
 </style>
